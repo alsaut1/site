1381ad3eaad8a159ba3649614f539c597a9d47cd
Alsaut <alsaut@seznam.cz>
2022-04-03 00:20:30 +0200

    2.21

--- init.php
+++ init.php
@@ -1,6 +1,11 @@
 <?php
-$con = mysqli_connect("localhost", "alsaut", "32768", "yeticave");
-mysqli_set_charset($con, "utf8");
-if ($con == false){
-  print("Ошибка подключения ". mysqli_connect_error());
+// $con = mysqli_connect("localhost", "alsaut", "32768", "yeticave");
+// mysqli_set_charset($con, "utf8");
+// if ($con == false){
+//   print("Ошибка подключения ". mysqli_connect_error());
+// }
+
+$con = new mysqli("localhost", "alsaut", "32768", "yeticave");
+if ($con ->connect_error){
+die('Error : ('. $con ->connect_errno . ')'.$con -> connect_error);
 }
